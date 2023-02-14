435b4457696f7022eee0f7d5d9bd4593b68e6823
yeomseunghyun <yumjja@gmail.com>
2023-02-14 16:46:27 +0900

    Initial Commit

---
+++ JustToIt/JustToItApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustToItApp.swift
+//  JustToIt
+//
+//  Created by 염승현 on 2023/02/14.
+//
+
+import SwiftUI
+
+@main
+struct JustToItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
